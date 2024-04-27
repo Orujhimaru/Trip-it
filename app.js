adcccec1d4fc4a68c002d0728975ef3813c36a92
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-04-28 02:15:13 +0300

    the work is done .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let allProducts = null;
+let allForecasts = [];
 
 async function getAllProducts() {
   try {
@@ -6,13 +6,27 @@ async function getAllProducts() {
       //   "http://api.weatherapi.com/v1/current.json?key=486b6d8f617f4184ade145624242704&q=Rome"
       "http://api.weatherapi.com/v1/forecast.json?key=486b6d8f617f4184ade145624242704&q=Rome&days=5"
     );
-    const products = await res.json();
-    allProducts = products;
+    const forecast = await res.json();
+    allForecasts = forecast.forecast.forecastday;
   } catch (error) {
     console.error(error);
   }
-  console.log(allProducts);
-  console.log(allProducts.current);
-  console.log(allProducts.current.temp_c);
+  console.log(allForecasts);
+  updateTemperature();
 }
 getAllProducts();
+
+function updateTemperature() {
+  for (let i = 0; i < 5; i++) {
+    let temp = document.getElementById(`temp${i + 1}`);
+    temp.innerText = allForecasts[i].day.avgtemp_c.toFixed(0) + "°";
+  }
+}
+
+let previousButton = document.getElementById("0");
+
+function changeBgC(navpage) {
+  previousButton.classList.toggle("backgroundSelected");
+  navpage.classList.add("backgroundSelected");
+  previousButton = navpage;
+}
